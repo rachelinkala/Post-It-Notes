89c196e11377b062bfa8bc2a88def83deb7fbfc4
Rachel Inkala <rachelinkala@gmail.com>
2018-08-30 15:54:45 -0600

    nav bar and edit

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -21,10 +21,24 @@ class NotesController < ApplicationController
     end
   end
 
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+
+    if @note.update(note_params)
+      redirect_to notes_path
+    else
+      render :edit
+    end
+  end
+
   private 
 
-    def notes_params
-      param.require(:note).permit(:title, :body)
+    def note_params
+      params.require(:note).permit(:title, :body)
     end
 
 
